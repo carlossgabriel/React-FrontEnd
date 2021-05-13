db072306e17768fa1dd4760367c14c9266eb3e7d
Carlos Gabriel <cgsalbuquerque@gmail.com>
2021-05-12 21:42:16 -0300

    props study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header'
+import Header from "./components/Header";
 
 function App() {
-    return <Header />;
+  return (
+    // Properties can be passed through the component as seen above
+    <div>
+      <Header title="Teste" title2="Teste2" />
+      <Header>
+        <ul>
+          <li>123</li>
+          <li>345</li>
+          <li>678</li>
+        </ul>
+      </Header>
+    </div>
+  );
 
-    // return (
-    //     <div>
-    //         <Header/>
-    //         <Header/>
-    //     </div>
-    // );
-
-    // Fragments: when you don't wan't the elements to interfeer in your DOM,
-    // It will create individual divs without a parent div
-    // return (
-    //     <>
-    //         <Header/>
-    //         <Header/>
-    //     </>
-    // );
+  // Fragments: when you don't wan't the elements to interfeer in your DOM,
+  // It will create individual divs without a parent div
+  // return (
+  //     <>
+  //         <Header/>
+  //         <Header/>
+  //     </>
+  // );
 }
 
 export default App;
